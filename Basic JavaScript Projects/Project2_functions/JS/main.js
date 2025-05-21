1ef3fb80dd0c5083b232bc7f7470231c35571dd0
DanielSan0916 <jessan0916@gmail.com>
2025-05-20 21:13:30 -0600

    updated JS code

--- Basic JavaScript Projects/Project2_functions/JS/main.js
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -1,5 +1,5 @@
 function My_First_Function() {
-    var str = "This is the button text!";
+    var str = "This is the button text for the Functions Assignment!";
     var color = "blue"; // New variable to store color value
     document.getElementById("Button_Text").innerHTML = str;
     document.getElementById("Button_Text").style.color = color; // Using second variable to change text color
