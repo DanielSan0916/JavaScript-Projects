f57a71f0ba584796d0618ec8b43150ae9a882cca
DanielSan0916 <jessan0916@gmail.com>
2025-05-21 21:53:07 -0600

    added NaN infinity and negative infinity functions and updated HTML to display values

--- Basic JavaScript Projects/Project5_comparisons_type_coercion/JS/main.js
+++ Basic JavaScript Projects/Project5_comparisons_type_coercion/JS/main.js
@@ -1,7 +1,20 @@
 function type_of() {
-        document.getElementById("typeof").innerHTML = typeof 2;
+        document.getElementById("typeof").innerHTML = typeof 2; // This will return "number"
 }
 
 function type_coercion() {
-        document.getElementById("typecoercion").innerHTML = 2 + "2";
+        document.getElementById("typecoercion").innerHTML = 2 + "2"; // This will return "22" because the number is coerced to a string
 }
+
+function isNaN() {
+        document.getElementById("NaN").innerHTML = 0 / 0; // This will return NaN (Not a Number) because you cannot divide zero by zero
+}
+
+function infinity() {
+        document.getElementById("infinity").innerHTML = 2E310; // This will return Infinity because the number is too large
+}
+
+function -infinity() {
+        document.getElementById("negativeinfinity").innerHTML = -3E310; // This will return -Infinity because the number is too large in the negative direction
+}   
+
