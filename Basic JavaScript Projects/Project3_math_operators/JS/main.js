b4380afb2900de7f3fa21c6bda2cf59c0fd75640
DanielSan0916 <jessan0916@gmail.com>
2025-05-21 19:14:48 -0600

    corrected errors in code

--- Basic JavaScript Projects/Project3_math_operators/JS/main.js
+++ Basic JavaScript Projects/Project3_math_operators/JS/main.js
@@ -9,9 +9,9 @@ function subtraction_Function() {
 }
 function multiplication_Function() {
     var multiplication = 10 * 10; // Perform multiplication
-    document.getElementById("Math3").innerHTML = "10 * 10 = " + multiplication;
+    document.getElementById("Math3").innerHTML = "10 * 10 = " + simple_math;
 }
 function division_Function() {
     var division = 10 / 2; // Perform division
-    document.getElementById("Math4").innerHTML = "10 / 2 = " + division;
+    document.getElementById("Math4").innerHTML = "10 / 2 = " + simple_math;
 }
