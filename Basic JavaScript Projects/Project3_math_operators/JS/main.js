a7be2ea999de791cf4129cdc5aace555de3b0c80
DanielSan0916 <jessan0916@gmail.com>
2025-05-21 19:08:30 -0600

    corrected errors in the code

--- Basic JavaScript Projects/Project3_math_operators/JS/main.js
+++ Basic JavaScript Projects/Project3_math_operators/JS/main.js
@@ -1,4 +1,4 @@
 function addition_Function() {
-    var addition = 5 + 7; // Perform addition
+    var addition = 10 + 10; // Perform addition
     document.getElementById("Math").innerHTML = "10 + 10 = " + addition;
 }
