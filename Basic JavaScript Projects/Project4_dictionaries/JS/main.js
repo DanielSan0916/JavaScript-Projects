2376753db5f9a5c4cb632ea337b4af8bf6b22636
DanielSan0916 <jessan0916@gmail.com>
2025-05-21 20:45:27 -0600

    Added delete operator to JS and updated HTML

--- Basic JavaScript Projects/Project4_dictionaries/JS/main.js
+++ Basic JavaScript Projects/Project4_dictionaries/JS/main.js
@@ -1,4 +1,4 @@
-function my_Dictionary() {
+function dictionary_display() {
     // This function creates a dictionary (object) with car details and displays the make of the car
     // in an HTML element with the ID "Dictionary".
     var Car = {
@@ -8,4 +8,18 @@ function my_Dictionary() {
         "Color": "Blue"
     };
     document.getElementById("Dictionary").innerHTML = Car.Make;
+}
+
+function dictionary_delete() {
+    // This function creates a dictionary (object) with car details and displays the model of the car
+    // in an HTML element with the ID "Dictionary".
+    var Car = {
+        "Make": "Toyota",
+        "Model": "Corolla",
+        "Year": 2020,
+        "Color": "Blue"
+    };
+    delete Car.Color; // This line deletes the Color property from the Car object
+    // The following line will display the Model of the car in the HTML element with ID "Dictionary"
+    document.getElementById("DictionaryDelete").innerHTML = Car.Model;
 }
