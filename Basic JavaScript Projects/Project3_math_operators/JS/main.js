df187fc6b738719aeea4e37bdf27d1dc1a7f8146
DanielSan0916 <jessan0916@gmail.com>
2025-05-21 19:12:15 -0600

    added Subtraction function to JavaScript and updated the HTML

--- Basic JavaScript Projects/Project3_math_operators/JS/main.js
+++ Basic JavaScript Projects/Project3_math_operators/JS/main.js
@@ -2,3 +2,8 @@ function addition_Function() {
     var addition = 10 + 10; // Perform addition
     document.getElementById("Math").innerHTML = "10 + 10 = " + addition;
 }
+
+function subtraction_Function() {
+    var subtraction = 10 - 5; // Perform subtraction
+    document.getElementById("Math2").innerHTML = "10 - 5 = " + subtraction;
+}
