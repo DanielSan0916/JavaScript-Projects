906bbea9c32b757296c758bc71f6be6a46cf91b6
DanielSan0916 <jessan0916@gmail.com>
2025-05-21 21:20:29 -0600

    added type_coercion operator to JS

--- Basic JavaScript Projects/Project5_comparisons_type_coercion/JS/main.js
+++ Basic JavaScript Projects/Project5_comparisons_type_coercion/JS/main.js
@@ -1,3 +1,7 @@
- function type_of() {
+function type_of() {
         document.getElementById("typeof").innerHTML = typeof 2;
+}
+
+function type_coercion() {
+        document.getElementById("typecoercion").innerHTML = 2 + "2";
 }
