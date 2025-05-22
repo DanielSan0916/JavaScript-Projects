14c43cd8da996b76a382f8765b42b3eca0e9b7b7
DanielSan0916 <jessan0916@gmail.com>
2025-05-21 19:30:19 -0600

    added negation_operator function to javascript and updated HTML

--- Basic JavaScript Projects/Project3_math_operators/JS/main.js
+++ Basic JavaScript Projects/Project3_math_operators/JS/main.js
@@ -7,19 +7,28 @@ function subtraction_Function() {
     var subtraction = 10 - 5; // Perform subtraction
     document.getElementById("Math2").innerHTML = "10 - 5 = " + subtraction;
 }
+
 function multiplication() {
     var simple_Math = 10 * 10; // Perform multiplication
     document.getElementById("Math3").innerHTML = "10 * 10 = " + simple_Math;
 }
+
 function division() {
     var simple_Math = 10 / 2; // Perform division
     document.getElementById("Math4").innerHTML = "10 / 2 = " + simple_Math;
 }
+
 function more_Math() {  
     var simple_Math = (1 + 2) * 10 / 2 - 5; // Perform a more complex math operation
     document.getElementById("Math5").innerHTML = "1 plus 2, multiplied 10, divided by 2, minus 5 equals " + simple_Math;
 }
+
 function modulus_Operator() {          
     var simple_Math = 25 % 6; // Perform modulus operation
     document.getElementById("Math6").innerHTML = "When you divide 25 by 6 you have a remainder of: " + simple_Math;
+}
+
+function negation_Operator() {
+    var x = 10; // Define a variable
+    document.getElementById("Math7").innerHTML = -x; // Negate the variable
 }
