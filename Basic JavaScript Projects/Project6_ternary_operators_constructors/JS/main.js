07c53e15e0e63ababb06979f038683088ea2c556
DanielSan0916 <jessan0916@gmail.com>
2025-05-22 19:31:43 -0600

    completed Project 6 Assignment

--- Basic JavaScript Projects/Project6_ternary_operators_constructors/JS/main.js
+++ Basic JavaScript Projects/Project6_ternary_operators_constructors/JS/main.js
@@ -1,3 +1,4 @@
+//A function with HTML and JS usung a ternary operator with input from the browser
 function Ride_Function() {
     var Height, Can_ride;
     Height = document.getElementById("Height").value;
@@ -5,3 +6,31 @@ function Ride_Function() {
     document.getElementById("Ride").innerHTML = Can_ride + " to ride.";
 }
 
+//A constructor function utilizing "new" and "this" keywords
+function Vehicle(Make, Model, Year, Color) {
+    this.Vehicle_Make = Make;
+    this.Vehicle_Model = Model;
+    this.Vehicle_Year = Year;
+    this.Vehicle_Color = Color;
+}
+var Jack = new Vehicle("Dodge", "Viper", 2020, "Red");
+var Emily = new Vehicle("Jeep", "Trail Hawk", 2019, "White and Black");
+var Eric = new Vehicle("Ford", "Pinto", 1971, "Mustard");
+
+//A function to display the results of the constructor in an HTML element
+function myFunction() {
+    document.getElementById("Keywords_and_Constructors").innerHTML =
+        "Eric drives a " + Eric.Vehicle_Color + "-colored " + Eric.Vehicle_Model +
+        " manufactured in " + Eric.Vehicle_Year;
+}
+
+//A nested function
+function add_Strings() {
+    var start_string = "Hello";
+
+    function Adding(str) {
+        start_string = start_string + "" + str;
+    }
+    Adding(" World");
+    document.getElementById("Nested_Function").innerHTML = start_string;
+}
