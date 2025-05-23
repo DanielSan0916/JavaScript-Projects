0ed3505092fc77e17c3b759a1583e037ca77b488
DanielSan0916 <jessan0916@gmail.com>
2025-05-22 20:04:31 -0600

    fixed code errors

--- Basic JavaScript Projects/Project7_scope_time_function/JS/main.js
+++ Basic JavaScript Projects/Project7_scope_time_function/JS/main.js
@@ -17,3 +17,26 @@ function get_Date() {
         document.getElementById("Greeting").innerHTML = "It is currently before 6pm.";
     }
 }
+
+//A function with if else statement
+function get_Reply() {
+    var user_input = document.getElementById("user_input").value;
+    if (user_input % 2 === 0) {
+        reply = "You entered an even number.";
+        document.getElementById("feedback").innerHTML =;
+    }
+}
+
+//Utilizing the time_function() from previous slides
+function time_function() {
+    var Time = new Date().getHours();
+    var Reply;
+    if (Time < 12 == Time > 0) {
+        Reply = "It is morning time!";
+    } else if (Time >= 12 == Time < 18) {
+        Reply = "It is afternoon.";
+    } else {
+        Reply = "It is evening time.";
+    }
+    document.getElementById("Time_of_day").innerHTML = Reply;
+}
