97ee38a0821e2fc884b575d1e36f078c465273ce
DanielSan0916 <jessan0916@gmail.com>
2025-05-24 20:09:22 -0600

    wrote the JS code to display the values of the array

---
+++ Basic JavaScript Projects/Project10_loops_arrays/JS/main.js
@@ -0,0 +1,5 @@
+// This function demonstrates the use of a string
+function array_Function() {
+    let myArray = ["Apple", "Banana", "Cherry", "Date"];
+    document.getElementById("Array").innerHTML = myArray.join(", ");
+}
