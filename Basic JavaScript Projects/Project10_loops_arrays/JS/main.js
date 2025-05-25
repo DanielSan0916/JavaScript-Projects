52990f5305caaf45fd114fc3cd10479a22f45b8b
DanielSan0916 <jessan0916@gmail.com>
2025-05-24 20:13:48 -0600

    defined the loop and For Loop in the JS code

--- Basic JavaScript Projects/Project10_loops_arrays/JS/main.js
+++ Basic JavaScript Projects/Project10_loops_arrays/JS/main.js
@@ -1,4 +1,30 @@
-// This function demonstrates the use of a string
+// This function demonstrates the use of a loop
+function Call_Loop() {
+    let i = 0;
+    let result = "";
+
+    while (i < 5) {
+        result += "Loop iteration: " + i + "<br>";
+        i++;
+    }
+
+    document.getElementById("Loop").innerHTML = result;
+}
+
+// This function demonstrates the use of a for loop
+function for_Loop() {
+    let instruments = ["Guitar", "Piano", "Violin", "Drums", "Flute"];
+    let result = "";
+
+    for (let i = 0; i < instruments.length; i++) {
+        result += "Instrument: " + instruments[i] + "<br>";
+    }
+
+    document.getElementById("List_of_Instruments").innerHTML = result;
+}
+
+
+// This function demonstrates the use of an array
 function array_Function() {
     let myArray = ["Apple", "Banana", "Cherry", "Date"];
     document.getElementById("Array").innerHTML = myArray.join(", ");
