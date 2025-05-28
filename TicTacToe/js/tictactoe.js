26d723fb6e6f874ce15dd935cce0dd3d77d09fbb
DanielSan0916 <jessan0916@gmail.com>
2025-05-27 23:22:00 -0600

    completed checkwinconditions functions

--- TicTacToe/js/tictactoe.js
+++ TicTacToe/js/tictactoe.js
@@ -38,3 +38,33 @@ function placeXOrO(squareNumber) {
          }
     }
 }
+function checkWinConditions() {
+    if (arrayIncludes('0X', '1X', '2X')) { drawWinLine(50, 100, 558, 100); }
+    else if (arrayIncludes('3X', '4X', '5X')) { drawWinLine(50, 304, 558, 304); }
+    else if (arrayIncludes('6X', '7X', '8X')) { drawWinLine(50, 508, 558, 508); }
+    else if (arrayIncludes('0X', '3X', '6X')) { drawWinLine(100, 50, 100, 558); }
+    else if (arrayIncludes('1X', '4X', '7X')) { drawWinLine(304, 50, 304, 558); }
+    else if (arrayIncludes('2X', '5X', '8X')) { drawWinLine(508, 50, 508, 558); }
+    else if (arrayIncludes('6X', '4X', '2X')) { drawWinLine(50, 558, 558, 50); }
+    else if (arrayIncludes('0X', '4X', '8X')) { drawWinLine(50, 50, 558, 558); }
+    else if (arrayIncludes('0o', '1O', '2O')) { drawWinLine(50, 100, 558, 100); }
+    else if (arrayIncludes('3O', '4O', '5O')) { drawWinLine(50, 304, 558, 304); }
+    else if (arrayIncludes('6O', '7O', '8O')) { drawWinLine(50, 508, 558, 508); }
+    else if (arrayIncludes('0O', '3O', '6O')) { drawWinLine(100, 50, 100, 558); }
+    else if (arrayIncludes('1O', '4O', '7O')) { drawWinLine(304, 50, 304, 558); }
+    else if (arrayIncludes('2O', '5O', '8O')) { drawWinLine(508, 50, 508, 558); }
+    else if (arrayIncludes('6O', '4O', '2O')) { drawWinLine(50, 558, 558, 50); }
+    else if (arrayIncludes('0O', '4O', '8O')) { drawWinLine(50, 50, 558, 558); }
+    else if (selectedSquares.length >= 9) {
+        audio('./media/tie.mp3');
+        setTimeout(function () { resetGame(); }, 500);
+    }
+    function arrayIncludes(squareA, squareB, squareC) {
+        const a = selectedSquares.includes(squareA);
+        const b = selectedSquares.includes(squareB);
+        const c = selectedSquares.includes(squareC);
+        if (a === true && b === true && c === true) {
+            return true;
+        }
+    }
+}
